823e02ab5c4d9f79eafee24e0b1868eab612886b
Fireworks-o <77235767+SyJin-pro@users.noreply.github.com>
2025-01-09 10:01:33 +0800

    Update main.py

--- WearLevelingUI/main.py
+++ WearLevelingUI/main.py
@@ -1,13 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow,QApplication,QWidget
-from Ui_wear_leveling import Ui_Form  #导入你写的界面类
+from Ui_wear_leveling import Ui_WearLeveling  #导入你写的界面类
+#from UI import Ui_Form
  
- 
-class MyMainWindow(QMainWindow,Ui_Form): 
+class MyMainWindow(QMainWindow,Ui_WearLeveling): 
     def __init__(self,parent =None):
         super(MyMainWindow,self).__init__(parent)
         self.setupUi(self)
- 
+        self.setWindowTitle("磨损均衡")  # 设置左上角标题
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWin = MyMainWindow()
